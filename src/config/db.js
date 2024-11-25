22d67e8fd269d342f7486bc475ea9332815a1e11
dharmesh1512 <dharmeshdiyora.dev@gmail.com>
2024-11-25 13:32:45 +0530

    update mongodb url

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,14 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+const mongoDB_URI =
+  process.env.MONGODB_URI ||
+  "mongodb+srv://admin:Evolv@109@sawankumar.xxce6.mongodb.net/";
+
 const connectDB = async () => {
   try {
     // Connect to MongoDB using the URI from the environment variable
-    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+    const connection = await mongoose.connect(mongoDB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
