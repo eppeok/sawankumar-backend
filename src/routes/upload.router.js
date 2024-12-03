26641ca7b72a36e77799351c25d9835c6839573d
dharmesh1512 <dharmeshdiyora.dev@gmail.com>
2024-12-03 11:34:44 +0530

    add cloudinary integration for image uploads and update dependencies

--- src/routes/upload.router.js
+++ src/routes/upload.router.js
@@ -2,33 +2,67 @@
 
 const express = require('express');
 const router = express.Router();
-const uploadConfig = require('../config/uploadConfig');
-const { handleUploadError } = require('../middleware/uploadMiddleware');
+const uploadConfig = require("../config/uploadConfig");
+const { handleUploadError } = require("../middleware/uploadMiddleware");
+const cloudinary = require("cloudinary").v2;
 
-// Single file upload route
-router.post('/single', 
-  uploadConfig.single('image'),
+cloudinary.config({
+  cloud_name: "dyljwyytr",
+  api_key: "189479382335773",
+  api_secret: "rL_4jo9vi2mt-CJc_jmPg6ePzL8",
+});
+
+// // Single file upload route -- npt needed
+// router.post(
+//   "/single",
+//   uploadConfig.single("image"),
+//   handleUploadError,
+//   (req, res) => {
+//     try {
+//       if (!req.file) {
+//         return res.status(400).json({
+//           success: false,
+//           error: "No file uploaded",
+//         });
+//       }
+
+//       const fileUrl = `/uploads/images/${req.file.filename}`;
+
+//       res.json({
+//         success: true,
+//         message: "File uploaded successfully",
+//         path: fileUrl,
+//         file: {
+//           filename: req.file.filename,
+//           mimetype: req.file.mimetype,
+//           size: req.file.size,
+//         },
+//       });
+//     } catch (error) {
+//       res.status(500).json({
+//         success: false,
+//         error: "Error uploading file",
+//         details: error.message,
+//       });
+//     }
+//   }
+// );
+
+// Upload image
+router.post(
+  "/single",
+  uploadConfig.single("image"),
   handleUploadError,
-  (req, res) => {
+  async (req, res) => {
     try {
-      if (!req.file) {
-        return res.status(400).json({ 
-          success: false, 
-          error: 'No file uploaded' 
-        });
-      }
-
-      const fileUrl = `/uploads/images/${req.file.filename}`;
+      const result = await cloudinary.uploader.upload(req.file.path, {
+        public_id: req.file.originalname,
+      });
 
       res.json({
         success: true,
-        message: 'File uploaded successfully',
-        path: fileUrl,
-        file: {
-          filename: req.file.filename,
-          mimetype: req.file.mimetype,
-          size: req.file.size
-        }
+        path: result.secure_url,
+        public_id: result.public_id,
       });
     } catch (error) {
       res.status(500).json({
