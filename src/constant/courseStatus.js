1b0742acc6a11fe8d526ab6ca0a15079692e8eb4
dharmesh1512 <dharmeshdiyora.dev@gmail.com>
2025-02-28 16:26:42 +0530

    init

--- src/constant/courseStatus.js
+++
@@ -1,6 +0,0 @@
-const COURSE_STATUS = {
-  PUBLISH: "published",
-  DRAFT: "draft",
-};
-
-module.exports = { COURSE_STATUS };
--- src/routes/customer.router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const Controller = require('../controllers/customer.controller');
-const router = express.Router();
-
-// Route to insert customer contact information
-router.post('/insert-contact', Controller.insertContact);
-
-module.exports = router;
